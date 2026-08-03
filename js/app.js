1311e40047a73d5de13791787fc72e35f46e0737
mohamed-ABannai <moabdulla172@gmail.com>
2026-08-03 19:43:24 +0300

    complete tic tac lab

--- js/app.js
+++ js/app.js
@@ -1,63 +1,165 @@
-//1) Define the required variables used to track the state of the game.
-
-//2) Store cached element references.
-
-//3) Upon loading, the game state should be initialized, and a function should 
-//   be called to render this game state.
-
-//4) The state of the game should be rendered to the user.
-
-//5) Define the required constants.
-
-//6) Handle a player clicking a square with a `handleClick` function.
-
-//7) Create Reset functionality.
-
-
-
-
 /*-------------------------------- Constants --------------------------------*/
 
-const choice=['X', 'O']
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board= ['','','','','','','','','']
-let turn='X'
-let winner=false
-let tie=false
+let board = ['', '', '', '', '', '', '', '', '']
+let turn = 'X'
+let winner = false
+let tie = false
+
+
+
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEls=document.querySelectorAll('.sqr')
+const squareEls = document.querySelectorAll('.sqr')
 
-const messageEl=document.querySelector('#message')
+const messageEl = document.querySelector('#message')
 
-console.log(messageEl)
+const resetBtnEl = document.querySelector('#resetBtnEl')
+
 /*-------------------------------- Functions --------------------------------*/
-function init(){
+function init() {
 
-console.log('confirm')
+    board = ['', '', '', '', '', '', '', '', '']
+    turn = 'X'
+    winner = false
+    tie = false
+
+    render()
 }
 
 
-function render(){
+function render() {
 
+    updateBoard()
+    updateMessage()
 
 }
 
-function updateBoard(){
 
-    for(let i=0; i< board.length; i++){
-   
-    board[i]=squareEls
-    
+
+
+function updateBoard() {
+
+    for (let i = 0; i < board.length; i++) {
+
+        squareEls[i].textContent = board[i]
     }
 
 }
 
 
+function updateMessage() {
 
-console.log(updateBoard(board[1]))
+    if (!winner && !tie) {
+
+        messageEl.textContent = `${turn}'s turn`
+
+    } else if (!winner && tie) {
+
+        messageEl.textContent = `It's a tie!`
+
+    } else {
+
+        messageEl.textContent = `${turn} wins!`
+
+    }
+
+}
+
+function handleClick(event) {
+
+    const squareIndex = event.target.id
+
+    if (board[squareIndex] === 'X' || board[squareIndex] === 'O' || winner) {
+        return
+    }
+
+    placePiece(squareIndex)
+    checkForWinner()
+    checkForTie()
+    switchPlayerTurn()
+    render()
+
+}
+
+function placePiece(index) {
+
+    board[index] = turn
+
+}
+
+
+function checkForWinner() {
+
+    for (let i = 0; i < winningCombos.length; i++) {
+
+        const [a, b, c] = winningCombos[i]
+
+        if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
+
+            winner = true
+
+        }
+
+    }
+
+}
+
+function checkForTie() {
+
+    if (winner) {
+        return
+    }
+
+    let boardFull = true
+
+    for (let i = 0; i < board.length; i++) {
+
+        if (board[i] === '') {
+            boardFull = false
+        }
+
+    }
+
+    if (boardFull) {
+        tie = true
+    }
+
+}
+
+function switchPlayerTurn() {
+
+    if (winner) {
+        return
+    }
+
+    if (turn === 'X') {
+        turn = 'O'
+    } else {
+        turn = 'X'
+    }
+
+}
+
+
 /*----------------------------- Event Listeners -----------------------------*/
 
+for (let oneSquare of squareEls) {
+    oneSquare.addEventListener('click', handleClick)
+}
 
+resetBtnEl.addEventListener('click', init)
 
+init()
